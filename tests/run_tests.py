57bc204822832a6c23ac7883e5392f4da6f4ca37
Santiago Dueñas <sduenas@bitergia.com>
2015-08-18 18:11:40 +0200

    [tests] Set up unit tests framework

---
+++ tests/run_tests.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2015 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+import sys
+import unittest
+
+
+if __name__ == '__main__':
+    test_suite = unittest.TestLoader().discover('.', pattern='test*.py')
+    result = unittest.TextTestRunner(buffer=True).run(test_suite)
+    sys.exit(not result.wasSuccessful())
