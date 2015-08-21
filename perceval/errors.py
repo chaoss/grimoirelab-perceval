4e34582f9cfd8ec64841d5db5333c598a4082632
Santiago Dueñas <sduenas@bitergia.com>
2015-08-21 12:26:51 +0200

    [errors] Add Perceval base error class

---
+++ perceval/errors.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2015 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+class BaseError(Exception):
+    """Base class for Perceval exceptions.
+
+    Derived classes can overwrite the error message declaring ``message``
+    property.
+    """
+    message = 'Perceval base error'
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.msg = self.message % kwargs
+
+    def __str__(self):
+        return self.msg
---
+++ tests/test_errors.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2015 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+import sys
+
+if not '..' in sys.path:
+    sys.path.insert(0, '..')
+
+import unittest
+
+from perceval.errors import BaseError
+
+
+# Mock classes to test BaseError class
+class MockErrorNoArgs(BaseError):
+    message = "Mock error without args"
+
+
+class MockErrorArgs(BaseError):
+    message = "Mock error with args. Error: %(code)s %(msg)s"
+
+
+class TestBaseError(unittest.TestCase):
+
+    def test_subblass_with_no_args(self):
+        """Check subclasses that do not require arguments.
+
+        Arguments passed to the constructor should be ignored.
+        """
+        e = MockErrorNoArgs(code=1, msg='Fatal error')
+
+        self.assertEqual("Mock error without args", str(e))
+
+    def test_subclass_args(self):
+        """Check subclasses that require arguments"""
+
+        e = MockErrorArgs(code=1, msg='Fatal error')
+
+        self.assertEqual("Mock error with args. Error: 1 Fatal error",
+                         str(e))
+
+    def test_subclass_invalid_args(self):
+        """Check when required arguments are not given.
+
+        When this happens, it raises a KeyError exception.
+        """
+        kwargs = {'code' : 1, 'error' : 'Fatal error'}
+        self.assertRaises(KeyError, MockErrorArgs, **kwargs)
+
+
+if __name__ == "__main__":
+    unittest.main()
