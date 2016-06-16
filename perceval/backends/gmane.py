687092fd8a0878e5fa9a6ab86e19247d5c740365
Santiago Dueñas <sduenas@bitergia.com>
2016-06-16 14:45:12 +0200

    [gmane] Add class to manage Gmane mailing lists

--- perceval/backends/gmane.py
+++ perceval/backends/gmane.py
@@ -24,9 +24,12 @@
 #
 
 import logging
+import os
+import posixpath
 
 import requests
 
+from .mbox import MailingList
 from ..errors import RepositoryError
 from ..utils import urljoin
 
@@ -34,7 +37,112 @@ from ..utils import urljoin
 logger = logging.getLogger(__name__)
 
 
+DEFAULT_OFFSET = 0
 MAX_MESSAGES = 2000 # Maximum number of messages per query
+
+
+class GmaneMailingList(MailingList):
+    """Manage mailing list archives stored by Gmane.
+
+    This class gives access to remote and local messages from a
+    mailing list stored by Gmane. Due to the nature of Gmane
+    and how messages are stored, this class does not manage
+    overlaped mboxes nor duplicated messages. Messages must be
+    filtered on later stages.
+
+    :param mailing_list_address: address of the mailing list (i.e:
+        my-mailing-list@example.com)
+    :param dirpath: path to the local mboxes archives
+
+    :raises  RepositoryError: when the given mailing list repository
+        is not stored by Gmane
+    """
+    def __init__(self, mailing_list_address, dirpath):
+        self.client = GmaneClient()
+        self._url = self.client.mailing_list_url(mailing_list_address)
+        super().__init__(self._url, dirpath)
+
+    def fetch(self, from_offset=DEFAULT_OFFSET):
+        """Fetch the messages from Gmane and store them in mbox files.
+
+        Stores the messages in mboxes files in the path given during the
+        initialization of this object. Messages are fetched from the given
+        offset.
+
+        :param from_offset: start to fetch messages from the given index
+
+        :returns: a list of tuples, storing the links and paths of the
+            fetched archives
+        """
+        logger.info("Downloading messages from '%s' and offset %s",
+                    self.url, str(from_offset))
+        logger.debug("Storing messages in '%s'", self.dirpath)
+
+        fetched = []
+        mailing_list = posixpath.basename(self.url)
+        offset = from_offset
+
+        while True:
+            messages = self.client.messages(mailing_list, offset,
+                                            max_messages=MAX_MESSAGES)
+
+            # In Gmane, an empty page means we reached the last message.
+            if len(messages) == 0:
+                break
+
+            filepath = os.path.join(self.dirpath, str(offset))
+
+            success = self._store_messages(filepath, offset, messages)
+
+            if success:
+                fetched.append((offset, filepath))
+
+            offset += MAX_MESSAGES
+
+        logger.info("Messages from Gmane %s downloaded", self.url)
+
+        return fetched
+
+    @property
+    def mboxes(self):
+        """Get the mboxes managed by this mailing list.
+
+        Returns the archives sorted by offset in ascending order.
+
+        :returns: a list of `.MBoxArchive` objects
+        """
+        archives = []
+
+        for mbox in super().mboxes:
+            try:
+                offset = int(os.path.basename(mbox.filepath))
+                archives.append((offset, mbox))
+            except ValueError:
+                logger.debug("Ignoring %s archive because its filename is not valid",
+                             mbox.filepath)
+                continue
+
+        archives.sort(key=lambda x: x[0])
+
+        return [a[1] for a in archives]
+
+    @property
+    def url(self):
+        """Gmane URL for this mailing list"""
+        return self._url
+
+    def _store_messages(self, filepath, offset, messages):
+        try:
+            with open(filepath, 'wb') as fd:
+                fd.write(messages)
+        except OSError as e:
+            logger.warning("Ignoring messages from %s with offset %s due to: %s",
+                           self.url, offset, str(e))
+            return False
+
+        logger.debug("%s messages with offset %s downloaded and stored in %s",
+                     self.url, offset, filepath)
+        return True
 
 
 class GmaneClient:
@@ -65,7 +173,7 @@ class GmaneClient:
 
         r = self.fetch(self.GMANE_DOWNLOAD_RTYPE, resource)
 
-        return r.text
+        return r.content
 
     def mailing_list_url(self, mailing_list_address):
         """Get the Gmane URL that stores the given mailing list address.
@@ -96,9 +204,8 @@ class GmaneClient:
                           'resource' : resource
                          }
 
-        logger.debug("Gmane client requests: %s resource: %s",
-                     rtype, resource)
-        print(url)
+        logger.debug("Gmane client requests: %s, rtype: %s resource: %s",
+                     url, rtype, resource)
 
         r = requests.get(url)
         r.raise_for_status()
--- tests/test_gmane.py
+++ tests/test_gmane.py
@@ -20,7 +20,10 @@
 #     Santiago Dueñas <sduenas@bitergia.com>
 #
 
+import os
+import shutil
 import sys
+import tempfile
 import unittest
 
 import httpretty
@@ -29,10 +32,12 @@ if not '..' in sys.path:
     sys.path.insert(0, '..')
 
 from perceval.errors import RepositoryError
-from perceval.backends.gmane import GmaneClient
+from perceval.backends.gmane import GmaneMailingList, GmaneClient
+from perceval.backends.mbox import MailingList
 
 
 GMANE_LIST_URL = 'http://list.gmane.org/mylist@example.com'
+GMANE_MYLIST_URL = 'http://dir.gmane.org/gmane.comp.example.mylist'
 GMANE_INVALID_LIST_URL = 'http://list.gmane.org/invalidlist@example.com'
 GMAME_DOWNLOAD_LIST_URL = 'http://download.gmane.org/gmane.comp.example.mylist'
 
@@ -43,6 +48,142 @@ def read_file(filename, mode='r'):
     return content
 
 
+def setup_http_server():
+    bodies = [read_file('data/gmane_messages.mbox', 'rb'),
+              read_file('data/gmane_messages_2000.mbox', 'rb'),
+              read_file('data/gmane_messages_4000.mbox', 'rb'),
+              read_file('data/gmane_messages_empty.mbox', 'rb')]
+
+    httpretty.register_uri(httpretty.GET, GMANE_LIST_URL,
+                           status=301,
+                           location=GMANE_MYLIST_URL)
+    httpretty.register_uri(httpretty.GET, GMANE_MYLIST_URL,
+                           status=200,
+                           body="")
+    httpretty.register_uri(httpretty.GET, GMAME_DOWNLOAD_LIST_URL + '/0/2000',
+                           status=200,
+                           body=bodies[0])
+    httpretty.register_uri(httpretty.GET, GMAME_DOWNLOAD_LIST_URL + '/2000/4000',
+                           status=200,
+                           body=bodies[1])
+    httpretty.register_uri(httpretty.GET, GMAME_DOWNLOAD_LIST_URL + '/4000/6000',
+                           status=200,
+                           body=bodies[2])
+    httpretty.register_uri(httpretty.GET, GMAME_DOWNLOAD_LIST_URL + '/6000/8000',
+                           status=200,
+                           body=bodies[3])
+
+
+class TestGmaneMailingList(unittest.TestCase):
+    """Tests for GmaneMailingList class"""
+
+    def setUp(self):
+        self.tmp_path = tempfile.mkdtemp(prefix='perceval_')
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_path)
+
+    @httpretty.activate
+    def test_init(self):
+        """Test if the mailing list values were initialized"""
+
+        setup_http_server()
+
+        gmls = GmaneMailingList('mylist@example.com', self.tmp_path)
+        self.assertIsInstance(gmls, MailingList)
+        self.assertEqual(gmls.uri, GMANE_MYLIST_URL)
+        self.assertEqual(gmls.dirpath, self.tmp_path)
+        self.assertEqual(gmls.url, GMANE_MYLIST_URL)
+
+    @httpretty.activate
+    def test_init_url_not_found(self):
+        """Test whether it raises an exception when a mailing list is not found"""
+
+        httpretty.register_uri(httpretty.GET, GMANE_INVALID_LIST_URL,
+                               status=200,
+                               body="No such list")
+
+        with self.assertRaises(RepositoryError):
+            _ = GmaneMailingList('invalidlist@example.com', self.tmp_path)
+
+    @httpretty.activate
+    def test_fetch(self):
+        """Test whether it fetches a set of messages"""
+
+        setup_http_server()
+
+        gmls = GmaneMailingList('mylist@example.com', self.tmp_path)
+
+        archives = gmls.fetch()
+        self.assertEqual(len(archives), 3)
+        self.assertEqual(archives[0][0], 0)
+        self.assertEqual(archives[0][1], os.path.join(self.tmp_path, '0'))
+        self.assertEqual(archives[1][0], 2000)
+        self.assertEqual(archives[1][1], os.path.join(self.tmp_path, '2000'))
+        self.assertEqual(archives[2][0], 4000)
+        self.assertEqual(archives[2][1], os.path.join(self.tmp_path, '4000'))
+
+        mboxes = gmls.mboxes
+        self.assertEqual(len(mboxes), 3)
+        self.assertEqual(mboxes[0].filepath, os.path.join(self.tmp_path, '0'))
+        self.assertEqual(mboxes[1].filepath, os.path.join(self.tmp_path, '2000'))
+        self.assertEqual(mboxes[2].filepath, os.path.join(self.tmp_path, '4000'))
+
+    @httpretty.activate
+    def test_fetch_from_offset(self):
+        """Test whether it fetches a set of messages from a given offset"""
+
+        setup_http_server()
+
+        gmls = GmaneMailingList('mylist@example.com', self.tmp_path)
+
+        archives = gmls.fetch(from_offset=4000)
+        self.assertEqual(len(archives), 1)
+        self.assertEqual(archives[0][0], 4000)
+        self.assertEqual(archives[0][1], os.path.join(self.tmp_path, '4000'))
+
+        mboxes = gmls.mboxes
+        self.assertEqual(len(mboxes), 1)
+        self.assertEqual(mboxes[0].filepath, os.path.join(self.tmp_path, '4000'))
+
+    @httpretty.activate
+    def test_fetch_empty(self):
+        """Test whether it does nothing when a response is empty"""
+
+        setup_http_server()
+
+        gmls = GmaneMailingList('mylist@example.com', self.tmp_path)
+
+        archives = gmls.fetch(from_offset=6000)
+        self.assertEqual(len(archives), 0)
+
+        mboxes = gmls.mboxes
+        self.assertEqual(len(mboxes), 0)
+
+    @httpretty.activate
+    def test_mboxes(self):
+        """Test whether it returns the mboxes ordered by the offset on their filenames"""
+
+        setup_http_server()
+
+        # Simulate the fetch process copying the files
+        shutil.copy('data/gmane_messages.mbox',
+                    os.path.join(self.tmp_path, '0'))
+        shutil.copy('data/gmane_messages_2000.mbox',
+                    os.path.join(self.tmp_path, '2000'))
+        shutil.copy('data/gmane_messages_4000.mbox',
+                    os.path.join(self.tmp_path, '4000'))
+        shutil.copy('data/gmane_messages_empty.mbox',
+                    os.path.join(self.tmp_path, 'gmane_messages_empty.mbox'))
+
+        gmls = GmaneMailingList('mylist@example.com', self.tmp_path)
+
+        mboxes = gmls.mboxes
+        self.assertEqual(mboxes[0].filepath, os.path.join(self.tmp_path, '0'))
+        self.assertEqual(mboxes[1].filepath, os.path.join(self.tmp_path, '2000'))
+        self.assertEqual(mboxes[2].filepath, os.path.join(self.tmp_path, '4000'))
+
+
 class TestGmaneClient(unittest.TestCase):
     """Tests for GmaneClient class"""
 
@@ -50,7 +191,7 @@ class TestGmaneClient(unittest.TestCase):
     def test_messages(self):
         """Test if a set of messages is fetched"""
 
-        body = read_file("data/gmane_messages.mbox")
+        body = read_file('data/gmane_messages.mbox', 'rb')
 
         url = GMAME_DOWNLOAD_LIST_URL + '/888/898'
 
@@ -67,20 +208,12 @@ class TestGmaneClient(unittest.TestCase):
     def test_mailing_list_url(self):
         """Test whether it returns the URL of a mailing list in Gmane"""
 
-        target = "http://dir.gmane.org/gmane.comp.example.mylist"
-
-        httpretty.register_uri(httpretty.GET, GMANE_LIST_URL,
-                               status=301,
-                               location=target)
-
-        httpretty.register_uri(httpretty.GET, target,
-                               status=200,
-                               body="")
+        setup_http_server()
 
         client = GmaneClient()
 
         url = client.mailing_list_url('mylist@example.com')
-        self.assertEqual(url, target)
+        self.assertEqual(url, GMANE_MYLIST_URL)
 
     @httpretty.activate
     def test_mailing_list_url_not_found(self):
