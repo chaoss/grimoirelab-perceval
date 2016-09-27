b76b0816329ae88f248358b553c178c8e7824ccb
shongoku <canibano_iglesias@hotmail.com>
2016-09-27 11:40:20 +0200

    Delete a line for debug.

--- perceval/backends/jenkins.py
+++ perceval/backends/jenkins.py
@@ -179,7 +179,6 @@ class JenkinsCommand(BackendCommand):
                 # We must do this for work with windows and Posix systems
                 base_path = os.path.expanduser("~")
                 base_path = os.path.join(base_path, ".perceval", "cache")
-                print("Valor de base_path: ", base_path)
             else:
                 base_path = self.parsed_args.cache_path
 
