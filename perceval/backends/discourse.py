95fe545a42fe70a108f73765afa9adf4dcfd18a4
Santiago Dueñas <sduenas@bitergia.com>
2016-10-25 11:51:37 +0200

    [discourse] Add 'has_resuming' class method

--- perceval/backends/discourse.py
+++ perceval/backends/discourse.py
@@ -239,6 +239,14 @@ class Discourse(Backend):
 
         return topics_ids
 
+    @classmethod
+    def has_resuming(cls):
+        """Returns whether it supports to resume the fetch process.
+
+        :returns: this backend supports items resuming
+        """
+        return True
+
     @staticmethod
     def metadata_id(item):
         """Extracts the identifier from a Discourse item."""
