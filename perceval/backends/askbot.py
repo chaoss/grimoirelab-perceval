a52570cb420001e521b7a1b0973794fc409bce1e
Alberto Martín <alberto.martin@bitergia.com>
2016-11-14 16:39:46 +0100

    [askbot] Add AskbotParser class

--- perceval/backends/askbot.py
+++ perceval/backends/askbot.py
@@ -21,8 +21,272 @@
 #
 
 import requests
+import re
+import bs4
+import logging
 
-from ..utils import urljoin
+from ..utils import urljoin, str_to_datetime
+
+logger = logging.getLogger(__name__)
+
+
+class AskbotParser:
+
+    def parse_question(self, question, html_question):
+        """Parse an Askbot API raw response.
+
+        The method parses the API question elements and adds
+        the extra information of the HTML question
+
+        :param question: item with the API question
+        :param html_question: HTML raw question
+
+        :returns: a generator of questions
+        """
+        # Initial case
+        logging.debug("Parsing question %s", question['id'])
+        bs_question = bs4.BeautifulSoup(html_question[0], "html.parser")
+        # Parse the user info from the soup container
+        question_container = self.parse_question_container(bs_question)
+        # Add the info to the question object
+        question.update(question_container)
+        # Add the comments of the question (if any)
+        if self.parse_question_comments(bs_question):
+            question['comments'] = self.parse_question_comments(bs_question)
+
+        answers = self.parse_answers(bs_question)
+
+        for page in range(2, len(html_question)+1):
+            logger.debug("Parsing pages of %s", question['id'])
+            position = page - 1
+            try:
+                html_question[position]
+            except IndexError:
+                continue
+            else:
+                bs_question = bs4.BeautifulSoup(html_question[position], "html.parser")
+                answers.extend(self.parse_answers(bs_question))
+
+        if len(answers) != 0:
+            question['answers'] = answers
+        return question
+
+    def parse_question_container(self, bs_question):
+        """Parse the question info container of a given HTML question.
+
+        The method parses the information available in the question information
+        container. The container can have up to 2 elements: the first one
+        contains the information related with the user who generated the question
+        and the date (if any). The second one contains the date of the updated,
+        and the user who updated it (if not the same who generated the question).
+
+        :param bs_question: beautiful soup question element
+
+        :returns: an object with the parsed information
+        """
+        container_info = {}
+        question = bs_question.select("div.js-question")
+        container = question[0].select("div.post-update-info")
+        created = container[0]
+        container_info['author'] = self.parse_user_info(created)
+        try:
+            container[1]
+        except IndexError:
+            pass
+        else:
+            updated = container[1]
+            if self.parse_user_info(updated):
+                container_info['updated_by'] = self.parse_user_info(updated)
+
+        return container_info
+
+    def parse_question_comments(self, bs_question):
+        """Parse the comments of a given HTML question.
+
+        The method parses the comments available for each question.
+
+        :param bs_question: beautiful soup question element
+
+        :returns: a list with the desired comments
+        """
+        question = bs_question.select("div.js-question")
+        comments = question[0].select("div.comment")
+        question_comments = self.parse_comments(comments)
+        return question_comments
+
+    def parse_answers(self, bs_question):
+        """Parse the answers of a given HTML question.
+
+        The method parses the answers related with a given HTML question,
+        as well as all the comments related to the answer.
+
+        :param bs_question: beautiful soup question element
+
+        :returns: a list with the answers
+        """
+        answer_list = []
+        # Select all the answers
+        bs_answers = bs_question.select("div.answer")
+        for bs_answer in bs_answers:
+            answer_id = bs_answer.attrs["data-post-id"]
+            votes_element = bs_answer.select("div.vote-number")[0].text
+            # Select all the comments in the answer
+            comments = self.parse_answer_comments(bs_answer)
+            # Select the body of the answer
+            body = bs_answer.select("div.post-body")
+            # Get the user information container and parse it
+            update_info = body[0].select("div.post-update-info")
+            answer_container = self.parse_answer_container(update_info)
+            # Remove the update-info-container div to be able to get the body
+            body[0].div.extract().select("div.post-update-info-container")
+            # Override the body with a clean one
+            body = body[0].get_text(strip=True)
+            # Generate the answer object
+            answer = {'id': answer_id,
+                      'score': votes_element,
+                      'summary': body
+                      }
+            if comments:
+                answer['comments'] = comments
+            # Update the object with the information in the answer container
+            answer.update(answer_container)
+            answer_list.append(answer)
+        return answer_list
+
+    def parse_answer_comments(self, bs_answer):
+        """Parse the comments of a given HTML answer.
+
+        The method parses the comments available for each answer.
+
+        :param bs_answer: beautiful soup answer element
+
+        :returns: a list with the desired comments
+        """
+        comments = bs_answer.select("div.comment")
+        answer_comments = self.parse_comments(comments)
+        return answer_comments
+
+    def parse_comments(self, comments):
+        """Parse the HTML comments information of a given list of them.
+
+        The method parses the information available inside each comment.
+
+        :param comments: beautiful soup list of comments
+
+        :returns: a list with the parsed comments
+        """
+        comments_list = []
+        for comment in comments:
+            added_at = comment.select("abbr.timeago")[0].attrs["title"]
+            element = {'added_at': str(str_to_datetime(added_at).timestamp()),
+                       'author': self.parse_comment_author(comment),
+                       'id': comment.attrs["data-comment-id"],
+                       'summary': comment.select("div.comment-body")[0].get_text(strip=True),
+                       'score': comment.select("div.upvote")[0].text
+                       }
+            comments_list.append(element)
+        return comments_list
+
+    @staticmethod
+    def parse_number_of_html_pages(bs_question):
+        """Parse number of answer pages to paginate over them.
+
+        :param bs_question: beautiful soup question element
+
+        :returns: an integer with the number of pages
+        """
+        try:
+            bs_question.select('div.paginator')[0]
+        except IndexError:
+            return 1
+        else:
+            return int(bs_question.select('div.paginator')[0].attrs['data-num-pages'])
+
+    @staticmethod
+    def parse_comment_author(comment):
+        """Parse the author information from an HTML comment.
+
+        The method parses the user information available inside a comment.
+
+        :param comment: beautiful soup comment
+
+        :returns: an object with the user information
+        """
+        username = comment.select("a.author")[0].text
+        href = comment.select("a.author")[0].attrs["href"]
+        user_id = re.search('\d+', href).group(0)
+        return {'id': user_id, 'username': username}
+
+    def parse_answer_container(self, update_info):
+        """Parse the answer info container of a given HTML question.
+
+        The method parses the information available in the answer information
+        container. The container can have up to 2 elements: the first one
+        contains the information related with the user who generated the question
+        and the date (if any). The second one contains the date of the updated,
+        and the user who updated it (if not the same who generated the question).
+
+        :param bs_question: beautiful soup answer container element
+
+        :returns: an object with the parsed information
+        """
+        container_info = {}
+        created = update_info[0]
+        answered_at = created.abbr.attrs["title"]
+        # Convert date to UNIX timestamp
+        container_info['added_at'] = str(str_to_datetime(answered_at).timestamp())
+        container_info['answered_by'] = self.parse_user_info(created)
+        try:
+            update_info[1]
+        except IndexError:
+            pass
+        else:
+            updated = update_info[1]
+            updated_at = updated.abbr.attrs["title"]
+            # Convert date to UNIX timestamp
+            container_info['updated_at'] = str(str_to_datetime(updated_at).timestamp())
+            if self.parse_user_info(updated):
+                container_info['updated_by'] = self.parse_user_info(updated)
+        return container_info
+
+    @staticmethod
+    def parse_user_info(update_info):
+        """Parse the user information of a given HTML container.
+
+        The method parses all the available user information in the container.
+        If the class "user-info" exists, the method will get all the available
+        information in the container. If not, if a class "tip" exists, it will be
+        a wiki post with no user associated. Else, it can be an empty container.
+
+        :param bs_question: beautiful soup answer container element
+
+        :returns: an object with the parsed information
+        """
+        user_info = {}
+        if update_info.select("div.user-info"):
+            # Get all the <a> elements in the container. First <a> contains the user
+            # information, second one (if exists), the website of the user.
+            elements = update_info.select("div.user-info")[0].find_all("a")
+            href = elements[0].attrs["href"]
+            user_info['id'] = re.search('\d+', href).group(0)
+            user_info['username'] = elements[0].text
+            user_info['reputation'] = update_info.select('span.reputation-score')[0].text
+            user_info['badges'] = update_info.select("span.badges")[0].attrs["title"]
+            try:
+                elements[1]
+            except IndexError:
+                pass
+            else:
+                user_info['website'] = elements[1].attrs["href"]
+            if update_info.select("img.flag"):
+                flag = update_info.select("img.flag")[0].attrs["alt"]
+                user_info['country'] = re.sub("flag of ", "", flag)
+            return user_info
+        elif update_info.select("p.tip"):
+            user_info = "This post is a wiki"
+            return user_info
+        else:
+            return
 
 
 class AskbotClient:
--- tests/test_askbot.py
+++ tests/test_askbot.py
@@ -23,13 +23,15 @@
 import sys
 import unittest
 import requests
+import bs4
+import json
 
 import httpretty
 
 if not '..' in sys.path:
     sys.path.insert(0, '..')
 
-from perceval.backends.askbot import AskbotClient
+from perceval.backends.askbot import AskbotClient, AskbotParser
 
 ASKBOT_URL = 'http://example.com'
 ASKBOT_QUESTIONS_API_URL = ASKBOT_URL + '/api/v1/questions'
@@ -42,6 +44,252 @@ def read_file(filename, mode='r'):
     with open(filename, mode) as f:
         content = f.read()
     return content
+
+
+class TestAskbotParser(unittest.TestCase):
+    "Askbot parser tests"
+    def test_parse_question(self):
+        """Test question parser. This test covers the case when a question contains more than
+        ten answers and pagination is needed."""
+        abparser = AskbotParser()
+
+        page_1 = read_file('data/askbot/html_24396_multipage_openstack.html')
+        page_2 = read_file('data/askbot/html_24396_multipage_2_openstack.html')
+        page_3 = read_file('data/askbot/html_24396_multipage_3_openstack.html')
+        page_4 = read_file('data/askbot/html_24396_multipage_4_openstack.html')
+
+        question_api = json.loads(read_file('data/askbot/api_24396_openstack.json'))
+
+        html_question = [page_1, page_2, page_3, page_4]
+
+        parsed_question = abparser.parse_question(question_api, html_question)
+
+        self.assertEqual(len(parsed_question['answers']), len(parsed_question['answer_ids']))
+
+        self.assertIsNotNone(parsed_question['added_at'])
+        self.assertIsNotNone(parsed_question['answer_count'])
+        self.assertIsNotNone(parsed_question['author'])
+        self.assertIsNotNone(parsed_question['id'])
+        self.assertIsNotNone(parsed_question['last_activity_at'])
+        self.assertIsNotNone(parsed_question['last_activity_by'])
+        self.assertIsNotNone(parsed_question['score'])
+        self.assertIsNotNone(parsed_question['summary'])
+        self.assertIsNotNone(parsed_question['tags'])
+        self.assertIsNotNone(parsed_question['title'])
+        self.assertIsNotNone(parsed_question['url'])
+        self.assertIsNotNone(parsed_question['view_count'])
+
+    def test_parse_question_container(self):
+        """Test parse question container. This tests the full case when a question is, apart from
+        created, edited by another user"""
+        abparser = AskbotParser()
+
+        page = read_file('data/askbot/html_26830_comments_question_openstack.html')
+
+        html_question = [page]
+
+        bs_question = bs4.BeautifulSoup(html_question[0], "html.parser")
+
+        container_info = abparser.parse_question_container(bs_question)
+
+        expected_container = {
+                              'author': {
+                                         'badges': 'Ignacio Mulas has 4 gold badges, 6 silver badges and 9 bronze badges',
+                                         'reputation': '111',
+                                         'username': 'Ignacio Mulas',
+                                         'id': '5000'
+                                         },
+                              'updated_by': {
+                                            'website': 'http://maffulli.net/',
+                                            'badges': 'smaffulli has 36 gold badges, 67 silver badges and 100 bronze badges',
+                                            'reputation': '6898',
+                                            'username': 'smaffulli',
+                                            'id': '9'
+                                            }
+                            }
+        self.assertEqual(container_info, expected_container)
+
+    def test_parse_question_comments(self):
+        """Test comments retrieved from questions contains all its elements"""
+        abparser = AskbotParser()
+
+        page = read_file('data/askbot/html_26830_comments_question_openstack.html')
+
+        html_question = [page]
+
+        bs_question = bs4.BeautifulSoup(html_question[0], "html.parser")
+        parsed_comments = abparser.parse_question_comments(bs_question)
+        self.assertEqual(len(parsed_comments), 3)
+        for parsed_comment in parsed_comments:
+            self.assertIsNotNone(parsed_comment['id'])
+            self.assertIsNotNone(parsed_comment['score'])
+            self.assertIsNotNone(parsed_comment['summary'])
+            self.assertIsNotNone(parsed_comment['author'])
+            self.assertIsNotNone(parsed_comment['added_at'])
+
+    def test_parse_answers(self):
+        """Given a question, parse all the answers available (pagination included)"""
+        abparser = AskbotParser()
+
+        page = read_file('data/askbot/html_24396_multipage_openstack.html')
+
+        html_question = [page]
+
+        bs_question = bs4.BeautifulSoup(html_question[0], "html.parser")
+        parsed_answers = abparser.parse_answers(bs_question)
+        self.assertEqual(len(parsed_answers), 10)
+        for parsed_answer in parsed_answers:
+            self.assertIsNotNone(parsed_answer['id'])
+            self.assertIsNotNone(parsed_answer['score'])
+            self.assertIsNotNone(parsed_answer['summary'])
+
+    def test_parse_answer_comments(self):
+        """Iterate over all the comments"""
+        abparser = AskbotParser()
+        page = read_file('data/askbot/html_148_comments_answer_2_openstack.html')
+
+        html_question = [page]
+
+        bs_question = bs4.BeautifulSoup(html_question[0], "html.parser")
+        bs_answers = bs_question.select("div.answer")
+        comments_to_test = abparser.parse_answer_comments(bs_answers[1])
+        comments = bs_answers[1].select("div.comment")
+        parsed_comments = abparser.parse_comments(comments)
+        self.assertEqual(comments_to_test, parsed_comments)
+
+    def test_parse_comments(self):
+        """Given a list of comments, test all the elements about to be parsed"""
+        page = read_file('data/askbot/html_148_comments_answer_2_openstack.html')
+
+        html_question = [page]
+
+        bs_question = bs4.BeautifulSoup(html_question[0], "html.parser")
+        bs_answers = bs_question.select("div.answer")
+        comments = bs_answers[1].select("div.comment")
+        parsed_comments = AskbotParser.parse_comments(AskbotParser, comments)
+        expected_comment_0 = {
+                              'summary': "HI, are there any guide on debugging with eclipse and pydev with the latest branch. I have tried commented out eventlet.monkeypatch(os=False) and replaced it with eventlet.monkeypatch(all=False,socket=True,time=True,os=False) and added import sys;sys.path.append('path') but breakpoints are ignored",
+                              'author': {
+                                        'id': '451',
+                                        'username': 'sak'
+                                        },
+                              'id': '814',
+                              'added_at': '1367914127.0',
+                              'score': ''
+                              }
+        expected_comment_1 = {
+                              'summary': '@sakthanks for asking. I believe yours would be a very good new question, more than a comment here. Do you mind posting it as a new question?',
+                              'author': {
+                                        'id': '9',
+                                        'username': 'smaffulli'
+                                        },
+                              'id': '872',
+                              'added_at': '1367949923.0',
+                              'score': ''
+                              }
+        expected_comment_2 = {
+                              'summary': 'cool. I have posted this as a new question: https://ask.openstack.org/question/815/how-do-i-debug-nova-service-with-eclipse-and-pydev/',
+                              'author': {
+                                         'id': '451',
+                                         'username': 'sak'
+                                         },
+                              'id': '886',
+                              'added_at': '1368003922.0',
+                              'score': ''
+                              }
+        self.assertEqual(parsed_comments[0], expected_comment_0)
+        self.assertEqual(parsed_comments[1], expected_comment_1)
+        self.assertEqual(parsed_comments[2], expected_comment_2)
+        self.assertEqual(len(parsed_comments), 3)
+
+    def test_parse_number_of_html_pages(self):
+        """Get the number of html needed to retrieve all the answers of a given page"""
+        page = read_file('data/askbot/html_24396_multipage_openstack.html')
+
+        html_question = [page]
+
+        bs_question = bs4.BeautifulSoup(html_question[0], "html.parser")
+
+        pages = AskbotParser.parse_number_of_html_pages(bs_question)
+        self.assertEqual(pages, 4)
+
+    def test_parse_comment_author(self):
+        """Get the user id and username of the author of the comment"""
+        page = read_file('data/askbot/html_7893_answer_3_updated.html')
+
+        html_question = [page]
+
+        bs_question = bs4.BeautifulSoup(html_question[0], "html.parser")
+        question = bs_question.select("div.js-question")
+        comments = question[0].select("div.comment")
+        author = AskbotParser.parse_comment_author(comments[0])
+        self.assertEqual(author['id'], "625")
+        self.assertEqual(author['username'], "todofixthis")
+
+    def test_parse_answer_container(self):
+        """Test answer container parsing. The answer container can be one or two elements,
+        one containing the user (or wiki) info, and one optional one containing information
+        regarding updates in the answer."""
+
+        page = read_file('data/askbot/html_7893_answer_3_updated.html')
+
+        html_question = [page]
+
+        bs_question = bs4.BeautifulSoup(html_question[0], "html.parser")
+
+        bs_answers = bs_question.select("div.answer")
+        body = bs_answers[2].select("div.post-body")
+        update_info = body[0].select("div.post-update-info")
+        answer_container = AskbotParser.parse_answer_container(AskbotParser, update_info)
+        expected_answered_by = {
+                                'country': 'Germany',
+                                'reputation': '283',
+                                'id': '707',
+                                'username': 'Jtrain',
+                                'badges': 'Jtrain has 9 gold badges, 5 silver badges and 16 bronze badges'
+                                }
+        expected_updated_by = {
+                               'country': 'Chile',
+                               'reputation': '1242',
+                               'id': '625',
+                               'username': 'todofixthis',
+                               'badges': 'todofixthis has 35 gold badges, 28 silver badges and 50 bronze badges'
+                               }
+
+        self.assertEqual(answer_container['answered_by'], expected_answered_by)
+        self.assertEqual(answer_container['updated_by'], expected_updated_by)
+
+
+    def test_parse_user_info(self):
+        """Test user info parsing. User info can be a wiki post or a user. When a user, some
+        additional information can be added like country or website when available"""
+
+        page = read_file('data/askbot/askbot_question_multipage_1.html')
+
+        html_question = [page]
+
+        bs_question = bs4.BeautifulSoup(html_question[0], "html.parser")
+        # Test the user_info from the question which is a wiki post and not updated
+        question = bs_question.select("div.js-question")
+        container = question[0].select("div.post-update-info")
+        created = container[0]
+        author = AskbotParser.parse_user_info(created)
+        self.assertEqual(author, "This post is a wiki")
+
+        # Test the user_info from an item with country and website
+        page = read_file('data/askbot/html_country_and_website.html')
+        html_question = [page]
+        bs_question = bs4.BeautifulSoup(html_question[0], "html.parser")
+        bs_answers = bs_question.select("div.answer")
+        body = bs_answers[0].select("div.post-body")
+        update_info = body[0].select("div.post-update-info")
+        author = AskbotParser.parse_user_info(update_info[0])
+        self.assertEqual(author['id'], "1")
+        self.assertEqual(author['badges'], "Evgeny has 56 gold badges, 98 silver badges and 212 bronze badges")
+        self.assertEqual(author['reputation'], "14023")
+        self.assertEqual(author['username'], "Evgeny")
+        self.assertEqual(author['website'], "http://askbot.org/")
+        self.assertEqual(author['country'], "Chile")
 
 
 class TestAskbotClient(unittest.TestCase):
