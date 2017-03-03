f8e66cc7a47195a23043468b52a585853469f414
Santiago Dueñas <sduenas@bitergia.com>
2017-03-03 20:22:10 +0100

    Fix PEP8 code style errors in setup.py

--- setup.py
+++ setup.py
@@ -41,8 +41,8 @@ try:
     import pypandoc
     long_description = pypandoc.convert(readme_md, 'rst')
 except (IOError, ImportError):
-    print("Warning: pypandoc module not found, or pandoc not installed. " \
-            + "Using md instead of rst")
+    print("Warning: pypandoc module not found, or pandoc not installed. "
+          "Using md instead of rst")
     with codecs.open(readme_md, encoding='utf-8') as f:
         long_description = f.read()
 
@@ -60,28 +60,28 @@ setup(name="perceval",
       author_email="sduenas@bitergia.com",
       license="GPLv3",
       classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3'
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python :: 3'
       ],
       keywords="development repositories analytics git github bugzilla jira jenkins",
       packages=[
-        'perceval',
-        'perceval.backends',
-        'perceval.backends.core'
+          'perceval',
+          'perceval.backends',
+          'perceval.backends.core'
       ],
       namespaces=[
-        'perceval.backends'
+          'perceval.backends'
       ],
       install_requires=[
-        'python-dateutil>=2.6.0',
-        'requests>=2.7.0',
-        'beautifulsoup4>=4.3.2',
-        'feedparser>=5.1.3'
+          'python-dateutil>=2.6.0',
+          'requests>=2.7.0',
+          'beautifulsoup4>=4.3.2',
+          'feedparser>=5.1.3'
       ],
       scripts=[
-        'bin/perceval'
+          'bin/perceval'
       ],
       zip_safe=False)
