800029c3af38c93af04db10c9915afd9c20df54a
Valerio Cosentino <valcos@bitergia.com>
2017-10-25 13:23:31 +0200

    [tests] Use absolute path for discourse test calls

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -28,6 +28,6 @@ import unittest
 
 if __name__ == '__main__':
 
-    test_suite = unittest.TestLoader().discover(os.path.join(os.path.dirname(os.path.abspath(__file__)), "."), pattern='test_confluence.py')
+    test_suite = unittest.TestLoader().discover(os.path.join(os.path.dirname(os.path.abspath(__file__)), "."), pattern='test_discourse.py')
     result = unittest.TextTestRunner(buffer=True).run(test_suite)
     sys.exit(not result.wasSuccessful())
--- tests/test_discourse.py
+++ tests/test_discourse.py
@@ -23,6 +23,7 @@
 #
 
 import datetime
+import os
 import shutil
 import sys
 import tempfile
@@ -33,7 +34,7 @@ import pkg_resources
 
 # Hack to make sure that tests import the right packages
 # due to setuptools behaviour
-sys.path.insert(0, '..')
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 pkg_resources.declare_namespace('perceval.backends')
 
 from perceval.backend import BackendCommandArgumentParser
@@ -55,7 +56,7 @@ DISCOURSE_POST_URL_2 = DISCOURSE_SERVER_URL + '/posts/22.json'
 
 
 def read_file(filename, mode='r'):
-    with open(filename, mode) as f:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), filename), mode) as f:
         content = f.read()
     return content
 
