289db32ad1959abcbddb7f2fef23cfd568e1107d
Valerio Cosentino <valcos@bitergia.com>
2017-10-25 15:11:22 +0200

    [tests] Use absolute path for phabricator test calls

--- tests/test_phabricator.py
+++ tests/test_phabricator.py
@@ -21,19 +21,19 @@
 #
 
 import datetime
+import httpretty
 import json
+import os
+import pkg_resources
+import requests
 import shutil
 import sys
 import tempfile
 import unittest
 
-import httpretty
-import pkg_resources
-import requests
-
 # Hack to make sure that tests import the right packages
 # due to setuptools behaviour
-sys.path.insert(0, '..')
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 pkg_resources.declare_namespace('perceval.backends')
 
 from perceval.backend import BackendCommandArgumentParser
@@ -55,7 +55,7 @@ PHABRICATOR_USERS_URL = PHABRICATOR_API_URL + '/user.query'
 
 
 def read_file(filename, mode='r'):
-    with open(filename, mode) as f:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), filename), mode) as f:
         content = f.read()
     return content
 
