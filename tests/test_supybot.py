3d444dd05cc17ecd2b2cc202aaa0447b72851a7c
Valerio Cosentino <valcos@bitergia.com>
2018-01-29 09:28:08 +0100

    [supybot] Apply archive to Supybot backend
    This code enable archive functionalities to the Supybot backend to
    store fetched data in the future.

--- tests/test_supybot.py
+++ tests/test_supybot.py
@@ -81,6 +81,11 @@ class TestSupybotBackend(unittest.TestCase):
 
         self.assertEqual(Supybot.has_caching(), False)
 
+    def test_has_archiving(self):
+        """Test if it returns False when has_archiving is called"""
+
+        self.assertEqual(Supybot.has_archiving(), False)
+
     def test_has_resuming(self):
         """Test if it returns True when has_resuming is called"""
 
