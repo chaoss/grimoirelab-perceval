656fd46cb0c579378d5d06d5d8c9edbfe5641ef3
Valerio Cosentino <valcos@bitergia.com>
2018-02-28 14:53:21 +0100

    [askbot] Align fetch signature to the backend generic one
    This code aligns the signature of the fetch method to the one
    provided by the generic backend

--- perceval/backends/core/askbot.py
+++ perceval/backends/core/askbot.py
@@ -37,6 +37,7 @@ from ...backend import (Backend,
 from ...client import HttpClient
 from ...utils import DEFAULT_DATETIME
 
+CATEGORY_QUESTION = 'question'
 
 logger = logging.getLogger(__name__)
 
@@ -52,7 +53,7 @@ class Askbot(Backend):
     :param tag: label used to mark the data
     :param archive: archive to store/retrieve items
     """
-    version = '0.5.0'
+    version = '0.6.0'
 
     def __init__(self, url, tag=None, archive=None):
         origin = url
@@ -62,12 +63,13 @@ class Askbot(Backend):
         self.client = None
         self.ab_parser = AskbotParser()
 
-    def fetch(self, from_date=DEFAULT_DATETIME):
+    def fetch(self, category=CATEGORY_QUESTION, from_date=DEFAULT_DATETIME):
         """Fetch the questions/answers from the repository.
 
         The method retrieves, from an Askbot site, the questions and answers
         updated since the given date.
 
+        :param category: the category of items to fetch
         :param from_date: obtain questions/answers updated since this date
 
         :returns: a generator of items
@@ -76,7 +78,7 @@ class Askbot(Backend):
             from_date = DEFAULT_DATETIME
 
         kwargs = {'from_date': from_date}
-        items = super().fetch("question", **kwargs)
+        items = super().fetch(category, **kwargs)
 
         return items
 
@@ -124,7 +126,7 @@ class Askbot(Backend):
         This backend only generates one type of item which is
         'question'.
         """
-        return 'question'
+        return CATEGORY_QUESTION
 
     @staticmethod
     def metadata_id(item):
